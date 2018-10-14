a19b9e30bb6258239bf5e331b8833be6a6874b45
saurav <sauravpd58@gmail.com>
2018-10-14 18:29:46 +0530

    added EmplyeeTest

---
+++ testGit/src/main/java/org/automation/testGit/EmplyeeTest.java
@@ -0,0 +1,13 @@
+package org.automation.testGit;
+
+public class EmplyeeTest
+{
+	public static void main(String[] args) 
+	{
+		Employee emp=new Employee();
+		emp.setEmpName("Steve");
+		emp.setEmpNo(201);		
+		System.out.println(emp.getEmpName());
+
+	}
+}
